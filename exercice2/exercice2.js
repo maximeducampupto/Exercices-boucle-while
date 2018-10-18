39c2489fdd9ef3f3c6cbe526d63c825307c8f27a
Max <maximeducampupto@gmail.com>
2018-10-18 15:34:26 +0200

    Initial commit

---
+++ exercice2/exercice2.js
@@ -0,0 +1,15 @@
+/**
+ * Created by sstienface on 16/10/2018.
+ */
+
+
+
+/* Modifier le code ci dessous pour réaliser l'exercice */
+
+var i = 0;
+while (i < 10) {
+    document.getElementById('monDiv').innerHTML+="Ma variable i vaux "+i + '<br>';
+    i += 2  ;
+}
+
+
---
+++ exercice3/exercice3.js
@@ -0,0 +1,15 @@
+/**
+ * Created by sstienface on 16/10/2018.
+ */
+
+
+
+/* Modifier le code ci dessous pour réaliser l'exercice */
+
+var i = 20;
+while (i > 0) {
+    document.getElementById('monDiv').innerHTML+="Ma variable i vaux "+i + '<br>';
+    i--;
+}
+
+
